d381c686552faa9392d2e3e44150c8e1d520c150
melekmsakni <ouesletyyosra@gmail.com>
2023-02-21 17:24:36 +0100

    yosra Reserver v1

---
+++ projetjava/src/edu/pijava/controllers/ReserverEventController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pijava.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author ouesl
+ */
+public class ReserverEventController implements Initializable {
+
+    @FXML
+    private Button btnReserver;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void ajouterReservation(ActionEvent event) {
+    }
+    
+}
